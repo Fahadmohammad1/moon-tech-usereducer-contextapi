4367108251b8661de62536d07ea068ff05a864e7
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-12-02 22:56:40 +0600

    succefully got product

--- src/context/ProductProvider.js
+++ src/context/ProductProvider.js
@@ -1,40 +1,19 @@
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useReducer,
-  useState,
-} from "react";
-import { actionTypes } from "../state/ProductState/actionTypes";
-import {
-  productReducer,
-  initialState,
-} from "../state/ProductState/productReducer";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const PRODUCT_CONTEXT = createContext();
 
 const ProductProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(productReducer, initialState);
-
-  console.log(state);
-
+  console.log(children);
+  const [data, setData] = useState([]);
   useEffect(() => {
-    dispatch({ type: actionTypes.FETCHING_START });
-    fetch("http://localhost:5000/products")
+    fetch("products.json")
       .then((res) => res.json())
-      .then((data) =>
-        dispatch({ type: actionTypes.FETCHING_SUCCESS, payload: data.data })
-      )
-      .catch(() => {
-        dispatch({ type: actionTypes.FETCHING_ERROR });
-      });
+      .then((data) => setData(data));
   }, []);
 
   const value = {
-    state,
-    dispatch,
+    data,
   };
-
   return (
     <PRODUCT_CONTEXT.Provider value={value}>
       {children}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,13 @@
 import React from "react";
-import ProductCard from "../components/ProductCard";
+// import ProductCard from "../components/ProductCard";
 import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
-  const {
-    state: { products, loading, error },
-  } = useProducts();
-
-  let content;
-
-  if (loading) {
-    content = <p>Loading</p>;
-  }
-
-  if (error) {
-    content = <p>Something went wrong, please try again later</p>;
-  }
-
-  if (!loading && !error && products.length === 0) {
-    content = <p>Nothing to show, product list is empty</p>;
-  }
-
-  if (!loading && !error && products.length) {
-    content = products.map((product) => (
-      <ProductCard key={product._id} product={product} />
-    ));
-  }
-
+  const { data } = useProducts();
+  console.log(data);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      {content}
+    <div>
+      <h1>this is home</h1>
     </div>
   );
 };
